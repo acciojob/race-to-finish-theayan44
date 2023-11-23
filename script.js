2ba5192ea5d31f2a8769613efc94aab7745e87d3
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-11-23 19:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 window.promises = [];
+const p1 = new Promise((res, rej)=>{
+	setTimeout(res, 100, "p1");
+});
+const p2 = new Promise((res, rej)=>{
+	setTimeout(res, 200, "p2");
+});
+const p3 = new Promise((res, rej)=>{
+	setTimeout(res, 300, "p3");
+});
+const p4 = new Promise((res, rej)=>{
+	setTimeout(res, 400, "p4");
+});
+const p5 = new Promise((res, rej)=>{
+	setTimeout(res, 500, "p5");
+});
+promises.push(p1);
+promises.push(p2);
+promises.push(p3);
+promises.push(p4);
+promises.push(p5);
+
+Promise.any(promises).then((data)=>{
+	document.getElementById("output").innerText = data;
+});
 
 // Do not change the code above this
 // add your promises to the array `promises`
